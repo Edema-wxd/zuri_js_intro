ddd1f48279fc149e127ac88b8299b638b6838644
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-06-20 19:38:55 +0100

    reset the calc file

--- calc.js
+++ calc.js
@@ -1,2 +1,9 @@
-// The calculator should be able to perform basic operations like Addition, Subtraction, Multiplication, & Division
-
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  readline.question(`What's your name?`, name => {
+    readline.close();
+  });
+  console.log(`Hi ${name}!`);
