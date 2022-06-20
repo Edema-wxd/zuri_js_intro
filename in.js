f31ca23fdf76cd34e86e4531309e4dfd28c7622b
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-06-20 18:09:26 +0100

    created and finished in

---
+++ in.js
@@ -0,0 +1,7 @@
+//Write a javascript code that prints out your name, height, and country on the screen
+
+// assign input
+
+//print outputs
+console.log('Hello my name is Francis');
+console.log('I am 5"11 and i stay in Nigeria');
